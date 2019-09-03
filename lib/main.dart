0ebc6e5a3aa642476a95d07b31f7288dbf700c4e
Harshit Batra <payafterwork.in@gmail.com>
2019-09-04 04:29:30 +0530

    Product class, item, overview, basic display:

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screens/products_overview.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ecommerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductsOverview(),
+    );
+  }
+}
